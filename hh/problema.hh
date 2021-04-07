e2ab49ced0247abdf6d787fbdf753cf755cd19fb
Fernando <go3212@hotmail.com>
2021-04-07 18:30:47 +0200

    Renombrada la clase problema

--- hh/problema.hh
+++ hh/problema.hh
@@ -22,7 +22,7 @@ struct Attempts
 /** @class Problem
  *  @brief Class which stores problem's data.
  */
-class Problem
+class Problema
 {
 private:
     string id;
@@ -31,12 +31,12 @@ public:
     /** @brief Overloaded default class constructor.
      *  \post Null problem identifier. All other private <em>int</em> variables are set to zero.
      */ 
-    Problem();
+    Problema();
 
     /** @brief Overloaded class constructor.
      *  \post Problem identifier ('this->id') set to the constructor parameter ('id'). All other private <em>int</em> variables are set to zero.
      */ 
-    Problem(const string& id);
+    Problema(const string& id);
 };
 
 #endif
