7b67f61dbc5eba2f95a99cf6cf7b341056ca307b
Fernando <go3212@hotmail.com>
2021-04-26 22:26:12 +0200

    Comentarios y revisión

--- cpp/usuario.cc
+++ cpp/usuario.cc
@@ -3,32 +3,11 @@
 
 using namespace std;
 
-Usuario::Usuario()
-{
-    currentCourse = UserCourseData();
-    hasUserid = false;
-    isInscribed = false;
-}
-
-void Usuario::print_all_time_solved_problems() const
-{
-    int i = 0;
-    for (i = 0; i < allCourses.sizeCoursesVect - 1; ++i)
-    {
-        cout << allCourses.coursesVect[i].pid << ' ';
-    }
-    if (allCourses.sizeCoursesVect != 0) cout << allCourses.coursesVect[i].pid << endl;
-}
-
+// Funciones que no son de la clase.
 bool sort_vect(ProblemData& a, ProblemData& b)
 {
     return a.pid < b.pid;
 }
-
-/**
- *  \pre Al menos hay un nodo. 
- *  \post Devuelve los elementos no solucionados adyacenetes a nodos solucionados.
- */
 
 void get_session_available_problems(const BinTree<ProblemData>& problemTree, vector<ProblemData>& problemVector)
 {
@@ -52,20 +31,35 @@ void get_session_available_problems(const BinTree<ProblemData>& problemTree, vec
     }
 }
 
-int Usuario::available_problems(vector<ProblemData>& problemVect) const
+// Funciones de la clase.
+Usuario::Usuario()
 {
-    // Si no está inscrito a ningún curso, no es posible listar problemas a enviar (tenga o no).
-    if (!isInscribed) return -1;
+    currentCourse = UserCourseData();
+    hasUserid = false;
+    isInscribed = false;
+}
 
-    // Los problemas a enviar son aquellos que no han sido solucionados y que son precedidos por problemas solucionados
-    // Por defecto, el primer problema de una sesión se puede enviar. 
-    for (int i = 0; i < currentCourse.num_problemTree; ++i)
-    {
-        get_session_available_problems(currentCourse.problemTreeVector[i], problemVect);
-    }
-    sort(problemVect.begin(), problemVect.end(), sort_vect);
-    int size = problemVect.size();  // Optimizar
-    return size;
+Usuario::Usuario(const userid& uid)
+{
+    this->uid = uid;
+    currentCourse = UserCourseData();
+    hasUserid = true;
+    isInscribed = false;
+}
+
+bool Usuario::has_userid() const
+{
+    return hasUserid;
+}
+
+bool Usuario::is_inscribed() const
+{
+    return isInscribed;
+}
+
+courseid Usuario::inscribed_course_id() const
+{
+    return currentCourse.identifier;
 }
 
 bool Usuario::inscribe(const courseid& cid, const Curso& course, Sesiones& sessions)
@@ -81,26 +75,39 @@ bool Usuario::inscribe(const courseid& cid, const Curso& course, Sesiones& sessi
 
     // Incializamos los iteradores de sesion previamente por eficiencia.
     SessionMap::const_iterator sessionIter;
+    // Inv: todos los *courseSessionVectorBegin pertenecen al conjunto de 'Sesiones'. courseSessionBegin++ tiende a courseSessionVectorEnd.
     while (courseSessionVectorBegin != courseSessionVectorEnd)
     {   
+        // Seguro que la sesión a buscar existe.
         sessions.get_session(*courseSessionVectorBegin, sessionIter);
+        // Insertamos el árbol de problemas de la sesión en el vector de árboles de problemas del usuario.
         currentCourse.problemTreeVector.push_back(sessionIter->second.get_problemTree());
         ++currentCourse.num_problemTree;
         ++courseSessionVectorBegin;
     }
-
+    // Seguro que se ha inscrito.
     isInscribed = true;
     return true; 
 }
 
-bool Usuario::is_inscribed() const
+int Usuario::available_problems(vector<ProblemData>& problemVect) const
 {
-    return isInscribed;
-}
+    // Si no está inscrito a ningún curso, no es posible listar problemas a enviar (tenga o no).
+    if (!isInscribed) return -1;
 
-courseid Usuario::inscribed_course_id() const
-{
-    return currentCourse.identifier;
+    // Los problemas a enviar son aquellos que no han sido solucionados y que son precedidos por problemas solucionados
+    // Por defecto, el primer problema de una sesión se puede enviar. 
+    // Inv: cada iteración incrementa el tamaño de problemVect.
+    for (int i = 0; i < currentCourse.num_problemTree; ++i)
+    {
+        // Se insertan detras de problemVect los problemas a solucionar del problemTreeVector[i].
+        get_session_available_problems(currentCourse.problemTreeVector[i], problemVect);
+    }
+    // Ordenamos el vector de problemas crecientemente.
+    sort(problemVect.begin(), problemVect.end(), sort_vect);
+    // El tamaño del vector se devuelve para poder iterar sobre él.
+    int size = problemVect.size();  // Optimizar
+    return size;
 }
 
 void Usuario::write() const
@@ -110,10 +117,13 @@ void Usuario::write() const
     cout << ((isInscribed) ? currentCourse.identifier : 0) << ')';
 }
 
-Usuario::Usuario(const userid& uid)
-{
-    this->uid = uid;
-    currentCourse = UserCourseData();
-    hasUserid = true;
-    isInscribed = false;
-}
+
+// void Usuario::print_all_time_solved_problems() const
+// {
+//     int i = 0;
+//     for (i = 0; i < allCourses.sizeCoursesVect - 1; ++i)
+//     {
+//         cout << allCourses.coursesVect[i].pid << ' ';
+//     }
+//     if (allCourses.sizeCoursesVect != 0) cout << allCourses.coursesVect[i].pid << endl;
+// }
--- hh/usuario.hh
+++ hh/usuario.hh
@@ -28,69 +28,112 @@ private:
 
 public:
 
-    /** @brief Overloaded default class null constructor.
-     *  \post Null user id ('uid'). All other private variables are also undefined.
+    /** @brief Constructor por defecto de la clase.
+     *  \pre true.
+     *  \post Todas las variables son inicializadas excepto el identificador de usuario ('uid').
      */ 
     Usuario();
 
-    /** @brief Overloaded class constructor.
-     *  @param uid identificador de usuario ('userid')
-     *  \post Sets the user id ('this->uid') to the constructor parameter ('uid'). All other private variables are left undefined.
+    /** @brief Constructor sobrecargado de la clase.
+     *  @param uid identificador de usuario ('userid').
+     *  \pre true.
+     *  \post Todas las variables de la clase son inicializadas y el identificador de usuario es el parámetro.
      */ 
     Usuario(const userid& uid);
 
-    /** @brief Inscribe al usuario en un curso.
-     *  @param cid identificador del curso ('courseid').
-     *  \pre El usuario debe tener 'uid' != NULL. El curso debe existir.
-     *  \post Si el usuario no está inscrito en ningún curso se le inscribe al parámetro de la función y se devuelve ('true'), en caso opuesto, no se modifica la inscripción y se devuelve ('false').
-     */
-    bool inscribe(const courseid& cid, const Curso& course, Sesiones& sessions);
-
-    /** @brief Devuelve si el usuario está inscrito en un curso.
-     *  \post Devuelve 'true' si el usuario está inscrito y 'false' si no lo está.
-     */
-    bool is_inscribed() const;
-
-    int available_problems(vector<ProblemData>& problemVect) const;
-
-    /** @brief Devuelve si el usuario tiene identificador
-     *  \post Devuelve 'true' si el usuario tiene identificador y 'false' si no lo tiene.
+    /** @brief Función que indica si el usuario tiene identificador.
+     *  \pre true.
+     *  \post El usuario mantiene el estado que tenia anteriormente.
+     *  @return bool: 'true' si el usuario tiene identificador y 'false' si no lo tiene.
      */
     bool has_userid() const;
 
+    /** @brief Función que indica si el usuario está inscrito en un curso.
+     *  \pre true.
+     *  \post El usuario mantiene el estado que tenia anteriormente.
+     *  @return bool: 'true' si el usuario está inscrito y 'false' si no lo está.
+     */
+    bool is_inscribed() const;
+
+    /**
+     * @brief Función que indica el identificador del curso al que está inscrito el usuario.
+     * \pre El usuario debe estar inscrito en un curso.
+     * \post El usuario mantiene el estado que tenia anteriormente.
+     * @return courseid: devuelve el identificador del curso al que está inscrito el usuario.
+     */
     courseid inscribed_course_id() const;
 
-    /** @brief Devuelve la información de todos los cursos cursados por el usuario.
-     *  \post Devuelve un puntero no modificable que contiene una estructura 'AllCourseData'. Devuelve NULL si el usuario no está inscrito en un curso.
+    /** @brief Inscribe al usuario en un curso.
+     *  @param cid identificador del curso ('courseid').
+     *  @param course objeto de tipo 'Curso'.
+     *  @param sessions objeto del tipo 'Sesiones'.
+     *  \pre El usuario debe tener identificador. El curso al que se quiere inscribir debe existir en el objeto 'Curso' y todas sus sesiones 
+     *       deben pertenecer al conjunto de sesiones del objeto 'Sesiones' proporcionado por los parámetros.
+     *  \post Si el usuario no está inscrito en ningún curso se le inscribe al parámetro de la función y se devuelve ('true'), 
+     *        en caso opuesto, no se modifica la inscripción y se devuelve ('false').
+     *  @return bool: 'true' si se ha inscrito al usuario correctamente, 'false' si el usuario ya estaba inscrito en un curso.
      */
-    vector<UserCoursesData>& all_courses() const;
+    bool inscribe(const courseid& cid, const Curso& course, Sesiones& sessions);
 
-    void print_all_time_solved_problems() const;
-
-    /** @brief Devuelve la información del curso al que está inscrito el usuario.
-     *  \post Devuelve un puntero no modificable que contiene una estructura 'UserCourseData'. Devuelve NULL si el usuario no está inscrito en un curso.
+    /**
+     * @brief Función que sirve para "visualizar" los problemas que tiene el usuario disponibles a solucionar.
+     * @param problemVect objeto que contendrá todos los problemas disponibles (que cumple todos los requisitos) a solucionar por el usuario. 
+     * \pre true.
+     * \post El vector tiene tamaño superior o igual al inicial. Si el return es diferente de -1, el vector estará ordenado crecientemente, si el return es -1,
+     *       el vector no se habrá modificado.
+     * @return int: '-1' si el usuario no está inscrito en ningun curso. El tamaño del vector del parámetro tras la ejecución en cualquier otro caso.
      */
-    vector<UserCourseData>& current_course() const;
-
-    /** @brief Actualiza el estado del curso al enviar un problema.
-     *  @param pid identificador de problema ('problemid').
-     *  @param solved valor que indica si el problema ha sido solucionado correctamente o no ('bool').
-     *  \post Actualiza el número de intentos y la lista de problemas disponibles para el usuario. Si no quedan más problemas a solucionar, se desinscribre al usuario del curso.
-     *        Devuelve 'true' si se ha encontrado el problema en el curso y 'false' si no sse ha encontrado.
-     */
-    bool update_problems(const problemid& pid, const bool& solved);
-
-    /** @brief Almacena la información de un usuario.
-     *  \pre Los datos en 'stdin' deben estar ordenados correctamente: identificador ('userid').
-     *  \post Se almacena un identificador en el usuario y se establece a 'true' el parámetro implícito ('has_userid').
-     */
-    void read();
+    int available_problems(vector<ProblemData>& problemVect) const;
 
     /** @brief Imprime por pantalla información sobre el usuario
+     *  \pre true.
      *  \post Se imprime por pantalla la infomación del problema en este formato:
      *        uid(intentos totales, intentos exitosos, problemas intentados (número), courseid)
+     *  @return void.
      */
     void write() const;
+
+    //######################################//
+    //      FUNCIONES NO IMPLEMENTADAS      //
+    //######################################//
+
+    // /** @brief Almacena la información de un usuario.
+    //  *  \pre Los datos en 'stdin' deben estar ordenados correctamente: identificador ('userid').
+    //  *  \post Se almacena un identificador en el usuario y se establece a 'true' el parámetro implícito ('has_userid').
+    //  */
+    // void read();
+
+    // /** @brief Actualiza el estado del curso al enviar un problema.
+    //  *  @param pid identificador de problema ('problemid').
+    //  *  @param solved valor que indica si el problema ha sido solucionado correctamente o no ('bool').
+    //  *  \post Actualiza el número de intentos y la lista de problemas disponibles para el usuario. Si no quedan más problemas a solucionar, se desinscribre al usuario del curso.
+    //  *        Devuelve 'true' si se ha encontrado el problema en el curso y 'false' si no sse ha encontrado.
+    //  */
+    // bool update_problems(const problemid& pid, const bool& solved);
+
+    // /**
+    //  * @brief 
+    //  * 
+    //  */
+    // void print_all_time_solved_problems() const;
 };
+
+/**
+ * @brief Función con el propósito de ordenar un vector de 'ProblemData' crecientemente según el 'problemid'.
+ *        Se utiliza exclusivamente el la función 'sort()' de la libreria '<algorithm>'.
+ * @param a objeto del tipo 'ProblemData'.
+ * @param b objeto del tipo 'ProblemData'.
+ * @return true si a < b y false para el resto de casos.
+ */
+bool sort_vect(ProblemData& a, ProblemData& b);
+
+/** @brief Funcion que inserta en un vector los problemas a solucionar de un árbol de 'ProblemData'.
+ *  @param problemTree árbol binario de objetos del tipo 'BinTree<ProblemData>'. 
+ *  @param problemVector vector al que se le insertan los problemas disponibles a solucionar. Tipo de objeto 'vector<ProblemData>'.
+ *  \pre Al menos hay un nodo, el árbol no está vacío.
+ *  \post Devuelve los elementos no solucionados adyacenetes a nodos solucionados, los inserta en el parámetro 'problemVector'.
+ *  @return void
+ */
+void get_session_available_problems(const BinTree<ProblemData>& problemTree, vector<ProblemData>& problemVector);
 
 #endif
