7984fed8bed129df52f2e983de5ccd81ab77f8bc
Fernando <go3212@hotmail.com>
2021-05-04 17:06:30 +0200

    Comentarios añadidos y correccion

--- src/tipos.hh
+++ src/tipos.hh
@@ -104,7 +104,7 @@ public:
         return *this;
     }
 
-    T& operator[] (const int& index)
+    const T& operator[] (const int& index) const
     {
         return classVector[index];
     };
@@ -149,24 +149,53 @@ public:
  */
 struct UserCourseData
 {
-    courseid identifier;
-    Attempts totalAttempts;
-    vector<ProblemData> solvedProblems;
-    vector<BinTree<ProblemData>> problemTreeVector;
-    int numProblems;
-    int solvedProblemsSize;
-    int sizeProblemTreeVector;
+    courseid identifier;                            //!< Identificador de curso ('courseid'). Representa el curso al que pertence la información.
+    Attempts totalAttempts;                         //!< Intentos totales realizados al curso.
+    vector<ProblemData> solvedProblems;             //!< Registro de problemas solucionados del curso, desde que se inscribió el usuario. Con su respectiva información ('ProblemData'). 
+    vector<BinTree<ProblemData>> problemTreeVector; //!< Árbol de problemas del curso, con su respectiva información. (Cada elemento del vector representa un árbol de problemas).
+    int numProblems;                                //!< Número de problemas que tiene el curso. (Representa el tamaño máximo del vector 'solvedProblems')
+    int solvedProblemsSize;                         //!< Número de problemas solucionados por el usuario. (Representa el tamaño del vector 'solvedProblems')
+    int sizeProblemTreeVector;                      //!< Número de sesiones que tiene el curso. (Repesenta el tamaño del vector 'problemTreeVector')
 
+    /** @brief Constructor por defecto de la estructura.
+     *  \pre true.
+     *  \post Todos los valores son nulos (0 o vacío).
+     */
     UserCourseData();
 
+    /** @brief Constructor sobrecargado de la estructura.
+     *  @param cid identificador de curso ('courseid').
+     *  \pre true.
+     *  \post Todos los valores son nulos (0 o vacío), excepto el 'identifier' que será el parámetro del constructor.
+     */
     UserCourseData(const courseid& cid);
 
+    /** @brief Functión que devuelve los problemas restantes por solucionar.
+     *  \pre true.
+     *  \post No modifica ninguna variable de la estructura.
+     *  @return int: número de problemas no resueltos.
+     */
     int notsolved_problems () const;
 
+    /** @brief Función que actualiza el estado de un problema de un árbol de 'ProblemData'.
+     *  @param pid identificador del problema a actualizar. Tipo 'problemid'.
+     *  @param problemData objeto del tipo 'ProblemData' al que se le asignará los datos del problema a actualizar. (si se encuentra).
+     *  @param solved estado que tendrá el problema cuando se actualice. (true si el problema se soluciona y false si no se soluciona). Tipo 'bool'. 
+     *  @param problemTree árbol de 'ProblemData' a modificar.
+     *  \pre true.
+     *  \post se actualiza el árbol binario 'problemTree' y se almacena el valor del problema encontrado en problemData (si se encuentra).
+     *  @return bool: true si se ha encontrado el problema en en árbol (y se ha actualizado), false si no se ha encontrado el problema (y por ende, no se ha actualizado).
+     */
     bool update_tree_problem (const problemid& pid, ProblemData& problemData, const bool& solved, BinTree<ProblemData>& problemTree);
 
+    /** @brief Función con el propósito de actualizar el estado de un problema del curso.
+     *  @param pid problema a actualizar. Tipo 'problemid'.
+     *  @param isSolved estado que tendrá el problema cuando se actualice. (true si el problema se soluciona y false si no se soluciona). Tipo 'bool'. 
+     *  \pre El objeto debe tener identificador y el problema debe existir en el árbol de problemas ('problemTree').
+     *  \post Se actualizan todos los datos del problema a modificar.
+     *  @return ProblemData: datos del problema despues de ser modificado.
+     */
     ProblemData update_data (const problemid& pid, const bool& isSolved);
-
 };
 
 /** @struct UserCoursesData
